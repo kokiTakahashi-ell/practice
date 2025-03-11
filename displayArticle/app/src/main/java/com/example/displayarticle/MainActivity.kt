9a75d365fea4f7334531529564e8e0fcfc7fad16
k-takahashi <>
2025-03-11 15:46:07 +0900

    実践の練習問題完了

--- displayArticle/app/src/main/java/com/example/displayarticle/MainActivity.kt
+++ displayArticle/app/src/main/java/com/example/displayarticle/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.displayarticle
 
+import android.graphics.Typeface.BOLD
+import android.graphics.Typeface.defaultFromStyle
 import android.os.Bundle
 
 import androidx.activity.ComponentActivity
@@ -7,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,9 +28,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.displayarticle.ui.theme.DisplayArticleTheme
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,17 +44,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
+//                    Quadrant(
+//                        first1 = getString(R.string.first1),
+//                        first2 = getString(R.string.first2),
+//                        second1 = getString(R.string.second1),
+//                        second2 = getString(R.string.second2),
+//                        third1 = getString(R.string.third1),
+//                        third2 = getString(R.string.third2),
+//                        fourth1 = getString(R.string.fourth1),
+//                        fourth2 = getString(R.string.fourth2),
+//                        modifier = Modifier
+//                    )
+//                    ComposeQuadrantApp()
+                    Quadrant_Test(
+                        first1 = getString(R.string.first1),
+                        first2 = getString(R.string.first2),
+                        second1 = getString(R.string.second1),
+                        second2 = getString(R.string.second2),
+                        third1 = getString(R.string.third1),
+                        third2 = getString(R.string.third2),
+                        fourth1 = getString(R.string.fourth1),
+                        fourth2 = getString(R.string.fourth2),
+                        modifier = Modifier
+                    )
 //                    Article(
 //                        head = getString(R.string.heading),
 //                        content1 = getString(R.string.content1),
 //                        content2 = getString(R.string.content1),
 //                        modifier = Modifier
 //                    )
-                    TaskDone(
-                        task1 = getString(R.string.task1),
-                        task2 = getString(R.string.task2),
-                        modifier = Modifier
-                    )
+//                    TaskDone(
+//                        task1 = getString(R.string.task1),
+//                        task2 = getString(R.string.task2),
+//                        modifier = Modifier
+//                    )
                 }
             }
         }
@@ -57,47 +85,165 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Quadrant() {
-    Column(modifier = Modifier.fillMaxSize()) {
+fun ComposeQuadrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.first1),
+                description = stringResource(R.string.first2),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.second1),
+                description = stringResource(R.string.second2),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.third1),
+                description = stringResource(R.string.third2),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.fourth1),
+                description = stringResource(R.string.fourth2),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun Quadrant_Test(first1: String, first2: String, second1: String, second2: String, third1: String, third2: String, fourth1: String, fourth2: String, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxWidth()) {
         Row(modifier = Modifier.weight(1f)) {
-            Box(
+            Column (
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxSize()
-                    .background(Color.Red),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Quadrant 1", color = Color.White)
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xFFEADDFF)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                Text(
+                    text = first1,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = first2,
+                    textAlign = TextAlign.Justify,
+                    fontSize = TextUnit.Unspecified,
+                )
             }
-            Box(
+            Column (
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxSize()
-                    .background(Color.Green),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Quadrant 2", color = Color.White)
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xFFD0BCFF)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                Text(
+                    text = second1,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = second2,
+                    textAlign = TextAlign.Justify,
+                    fontSize = TextUnit.Unspecified,
+                )
             }
         }
         Row(modifier = Modifier.weight(1f)) {
-            Box(
+            Column (
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxSize()
-                    .background(Color.Blue),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Quadrant 3", color = Color.White)
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xFFB69DF8)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                Text(
+                    text = third1,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = third2,
+                    textAlign = TextAlign.Justify,
+                    fontSize = TextUnit.Unspecified,
+                )
             }
-            Box(
+            Column (
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxSize()
-                    .background(Color.Yellow),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Quadrant 4", color = Color.Black)
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xFFF6EDFF)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                Text(
+                    text = fourth1,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = fourth2,
+                    textAlign = TextAlign.Justify,
+                    fontSize = TextUnit.Unspecified,
+                )
             }
+
         }
     }
 }
@@ -130,6 +276,135 @@ fun TaskDone(task1: String, task2: String, modifier: Modifier = Modifier){
     }
 }
 
+
+
+
+@Composable
+fun Quadrant(first1: String, first2: String, second1: String, second2: String, third1: String, third2: String, fourth1: String, fourth2: String, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.weight(1f)) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFEADDFF)),
+                contentAlignment = Alignment.Center
+            ) {
+                Column (
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ){
+                    Text(
+                        text = first1,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = first2,
+                        textAlign = TextAlign.Justify,
+                        fontSize = TextUnit.Unspecified,
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFD0BCFF)),
+                contentAlignment = Alignment.Center
+            ) {
+                Column (
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ){
+                    Text(
+                        text = second1,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = second2,
+                        textAlign = TextAlign.Justify,
+                        fontSize = TextUnit.Unspecified,
+                    )
+                }
+            }
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFB69DF8)),
+                contentAlignment = Alignment.Center
+            ) {
+                Column (
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ){
+                    Text(
+                        text = third1,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = third2,
+                        textAlign = TextAlign.Justify,
+                        fontSize = TextUnit.Unspecified,
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFF6EDFF)),
+                contentAlignment = Alignment.Center
+            ) {
+                Column (
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ){
+                    Text(
+                        text = fourth1,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = fourth2,
+                        textAlign = TextAlign.Justify,
+                        fontSize = TextUnit.Unspecified,
+                    )
+                }
+            }
+        }
+    }
+}
+
 @Composable
 fun Article(head: String, content1: String, content2: String, modifier: Modifier = Modifier) {
     Column (modifier = Modifier) {
@@ -137,7 +412,7 @@ fun Article(head: String, content1: String, content2: String, modifier: Modifier
         Image(
             painter = image,
             contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
+//            modifier = Modifier.fillMaxWidth()
         )
         Text(
             text = head,
@@ -151,7 +426,7 @@ fun Article(head: String, content1: String, content2: String, modifier: Modifier
             textAlign = TextAlign.Justify,
             modifier = modifier
                 .padding(start = 16.dp, end = 16.dp)
-                .fillMaxWidth(),
+//                .fillMaxWidth(),
         )
         Text(
             text = content2,
@@ -159,7 +434,7 @@ fun Article(head: String, content1: String, content2: String, modifier: Modifier
             textAlign = TextAlign.Justify,
             modifier = modifier
                 .padding(16.dp)
-                .fillMaxWidth(),
+//                .fillMaxWidth(),
         )
     }
 }
@@ -168,14 +443,24 @@ fun Article(head: String, content1: String, content2: String, modifier: Modifier
 @Composable
 fun TestPreview() {
     DisplayArticleTheme {
-//        Article(
-//            head = "Jetpack Compose tutorial",
-//            content1 = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-//            content2 = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
-//        )
-        TaskDone(
-            task1 = "All tasks completed",
-            task2 = "Nice work!"
+        Article(
+            head = "Jetpack Compose tutorial",
+            content1 = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            content2 = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
         )
+//        TaskDone(
+//            task1 = "All tasks completed",
+//            task2 = "Nice work!"
+//        )
+//        Quadrant(
+//            first1 = "Text composable",
+//            first2 = "Displays text and follows the recommended Material Design guidelines.",
+//            second1 = "Image composable",
+//            second2 = "Creates a composable that lays out and draws a given Painter class object.",
+//            third1 = "Row composable",
+//            third2 = "A layout composable that places its children in a horizontal sequence.",
+//            fourth1 = "Column composable",
+//            fourth2 = "A layout composable that places its children in a vertical sequence."
+//        )
     }
 }
