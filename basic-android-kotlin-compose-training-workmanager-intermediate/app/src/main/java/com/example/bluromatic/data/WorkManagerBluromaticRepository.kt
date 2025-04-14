9d5a77d61084f02c938ba0d1defbc11f5e824f5b
k-takahashi <>
2025-04-14 17:30:45 +0900

    Unit 7　高度なWorkManagerとテスト　初期化

--- basic-android-kotlin-compose-training-workmanager-intermediate/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ basic-android-kotlin-compose-training-workmanager-intermediate/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,7 +19,6 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
-import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
@@ -35,8 +34,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
-    private var imageUri: Uri = context.getImageUri() // <- Add this
+    private var imageUri: Uri = context.getImageUri()
     private val workManager = WorkManager.getInstance(context)
+
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
 
     /**
@@ -44,18 +44,24 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      * @param blurLevel The amount to blur the image
      */
     override fun applyBlur(blurLevel: Int) {
-        //起点を作成してqueueに追加(cleanupWorker)
+        // Add WorkRequest to Cleanup temporary images
         var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
-        val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
-        blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
-        //起点の次であるBlurWorkerをqueueに追加
-        continuation = continuation.then(blurBuilder.build())
-        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>().build()
-        //　saveImageをqueueに追加
-        continuation = continuation.then(save)
-        // Start the work
-        continuation.enqueue()
 
+        // Add WorkRequest to blur the image
+        val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+        // Input the Uri for the blur operation along with the blur level
+        blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+
+        continuation = continuation.then(blurBuilder.build())
+
+        // Add WorkRequest to save the image to the filesystem
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .build()
+        continuation = continuation.then(save)
+
+        // Actually start the work
+        continuation.enqueue()
     }
 
     /**
