63b94b75572372a4b1a42338f052507d711c83a6
k-takahashi <>
2025-04-15 18:30:45 +0900

    Unit 8-1の途中

--- basic-android-kotlin-compose-training-juice-tracker-views 2/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-juice-tracker-views 2/app/build.gradle.kts
@@ -53,10 +53,15 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+    composeOptions{
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.16.0")
@@ -68,6 +73,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra["arch_lifecycle_version"]}")
     implementation("androidx.navigation:navigation-fragment-ktx:${rootProject.extra["nav_version"]}")
     implementation("androidx.navigation:navigation-ui-ktx:${rootProject.extra["nav_version"]}")
+    // Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.12.0")
